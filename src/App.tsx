0204874c56c9ee32e0b8e0fce7c57d2adc15f3ed
DANIEL <danielvaldes67945@gmail.com>
2026-07-17 19:37:02 -0400

    fix: cambiar BrowserRouter a HashRouter para evitar descarga de HTML al navegar

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import { cn } from "@/lib/utils";
 import { Navigation } from "./components/Navigation";
 import { OfflineProvider } from "./providers/OfflineProvider";
@@ -164,7 +164,7 @@ const App = () => (
       <OfflineProvider>
         <Toaster />
         <Sonner />
-        <BrowserRouter>
+        <HashRouter>
           <TimeframeProvider>
             <SidebarProvider>
               <AppContent />
